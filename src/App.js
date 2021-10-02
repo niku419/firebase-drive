5136a082a609e16d3163a0ea4c0e799a7f76726a
Niku_419 <2019198@iiitdmj.ac.in>
2021-10-02 22:46:42 +0530

    :rocket: deploying to firebase with github actions

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Home from './ReactFirebaseDrive/components/Home.jsx'
+
+function App() {
+
+  return (
+    <>
+      <Home/>
+    </>
+  )
+}
+
+export default App
